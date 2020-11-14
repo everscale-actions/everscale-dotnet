9cb974b13a30d40c0f9be9c2d4e3846d911d9eb1
Alexander Salamatov <ch1seL@ya.ru>
2020-11-14 23:44:38 +0300

    init generator

---
+++ src/ch1seL.TonNet.ClientGenerator/Generator.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Formatting;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Formatting;
+
+namespace Generator
+{
+    public static class Generator
+    {
+        public static void GenerateModule()
+        {
+            CompilationUnitSyntax cu = SyntaxFactory.CompilationUnit()
+                .AddUsings(
+                    SyntaxFactory.UsingDirective(SyntaxFactory.IdentifierName("System")),
+                    SyntaxFactory.UsingDirective(SyntaxFactory.IdentifierName("System.Collections.Generic")),
+                    SyntaxFactory.UsingDirective(SyntaxFactory.IdentifierName("System.Linq")),
+                    SyntaxFactory.UsingDirective(SyntaxFactory.IdentifierName("System.Text")),
+                    SyntaxFactory.UsingDirective(SyntaxFactory.IdentifierName("System.Threading.Tasks")));
+
+            NamespaceDeclarationSyntax localNamespace = SyntaxFactory.NamespaceDeclaration(SyntaxFactory.IdentifierName("test"));
+            ClassDeclarationSyntax localClass = SyntaxFactory
+                .ClassDeclaration("test")
+                .AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword));
+
+            localNamespace = localNamespace.AddMembers(localClass);
+
+
+            cu = cu.AddMembers(localNamespace);
+            var cw = new AdhocWorkspace();
+            cw.Options.WithChangedOption(CSharpFormattingOptions.IndentBraces, true);
+            SyntaxNode formattedNode = Formatter.Format(cu, cw, cw.Options);
+
+
+            using FileStream fileStream = new FileInfo("test.cs").OpenWrite();
+            using var sw = new StreamWriter(fileStream);
+            formattedNode.WriteTo(sw);
+        }
+    }
+}
---
+++ src/ch1seL.TonNet.ClientGenerator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Model;
+using Model.Domain;
+
+namespace Runner
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            Generator.Generator.GenerateModule();
+        }
+    }
+}
