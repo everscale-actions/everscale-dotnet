3795c8f27be7f0230ceb7ec2a65310f18e868c05
Alexander Salamatov <ch1seL@ya.ru>
2020-11-18 16:58:28 +0300

    fix readme

---
+++ src/ch1seL.TonNet.Client.Tests/DisposingTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using ch1seL.TonNet.Client.Models;
+using FluentAssertions;
+using Xunit;
+
+namespace ch1seL.TonNet.Client.Tests
+{
+    public class DisposingTests
+    {
+        [Fact(Timeout = 1000)]
+        public void TonClintDisposingWell()
+        {
+            var act = new Func<Task>(async () =>
+            {
+                var tonClient = new TonClient();
+                ResultOfGetApiReference res = await tonClient.Client.GetApiReference();
+
+                tonClient.Dispose();
+            });
+
+            act.Should().NotThrow();
+        }
+    }
+}
