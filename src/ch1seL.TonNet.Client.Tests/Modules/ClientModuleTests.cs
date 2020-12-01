6afce678262b06170dc098f9bdfd5ee1af9182a1
Alexander Salamatov <ch1seL@ya.ru>
2020-12-01 22:35:04 +0300

    check sdk version in tests exactly not regexp

--- src/ch1seL.TonNet.Client.Tests/Modules/ClientModuleTests.cs
+++ src/ch1seL.TonNet.Client.Tests/Modules/ClientModuleTests.cs
@@ -9,6 +9,7 @@ namespace ch1seL.TonNet.Client.Tests.Modules
     public class ClientModuleTests : IClassFixture<TonClientTestsFixture>
     {
         private readonly ITonClient _tonClient;
+
         public ClientModuleTests(TonClientTestsFixture fixture, ITestOutputHelper outputHelper)
         {
             _tonClient = fixture.CreateClient(outputHelper);
@@ -19,7 +20,7 @@ namespace ch1seL.TonNet.Client.Tests.Modules
         {
             ResultOfVersion result = await _tonClient.Client.Version();
 
-            result.Version.Should().MatchRegex(@"\d\.\d\.\d");
+            result.Version.Should().Be("1.1.2");
         }
 
         [Fact(Skip = "Release 1.1.2 has BuildNumber with 0")]
