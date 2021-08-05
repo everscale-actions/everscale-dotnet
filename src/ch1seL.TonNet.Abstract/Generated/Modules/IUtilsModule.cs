33fd7295aaf4afca921309564206a1e5a8e2d12c
Alexander Salamatov <asalamatov@appulate.com>
2021-08-05 15:33:54 +0300

    update to 1.20.1

--- src/ch1seL.TonNet.Abstract/Generated/Modules/IUtilsModule.cs
+++ src/ch1seL.TonNet.Abstract/Generated/Modules/IUtilsModule.cs
@@ -16,8 +16,8 @@ namespace ch1seL.TonNet.Abstract.Modules
         /// <summary>
         /// <para>Validates and returns the type of any TON address.</para>
         /// <para>Address types are the following</para>
-        /// <para>`0:919db8e740d50bf349df2eea03fa30c385d846b991ff5542e67098ee833fc7f7` - standart TON address most</para>
-        /// <para>commonly used in all cases. Also called as hex addres</para>
+        /// <para>`0:919db8e740d50bf349df2eea03fa30c385d846b991ff5542e67098ee833fc7f7` - standard TON address most</para>
+        /// <para>commonly used in all cases. Also called as hex address</para>
         /// <para>`919db8e740d50bf349df2eea03fa30c385d846b991ff5542e67098ee833fc7f7` - account ID. A part of full</para>
         /// <para>address. Identifies account inside particular workchain</para>
         /// <para>`EQCRnbjnQNUL80nfLuoD+jDDhdhGuZH/VULmcJjugz/H9wam` - base64 address. Also called "user-friendly".</para>
