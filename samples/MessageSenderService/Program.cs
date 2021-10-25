3ea32a9b18412778cea771e647510c0db11c5e0a
Alexander Salamatov <ch1seL@ya.ru>
2021-10-25 17:57:25 +0300

    use localhost:8080 port as default endpoint for samples and tests

--- samples/MessageSenderService/Program.cs
+++ samples/MessageSenderService/Program.cs
@@ -19,7 +19,7 @@ namespace SampleWorkerService
                     services.AddHostedService<Worker>();
                     services.AddTonClient(config =>
                     {
-                        config.Network.Endpoints = new[] {"http://localhost"};
+                        config.Network.Endpoints = new[] { "http://localhost:8080" };
                         config.Network.WaitForTimeout = 5000;
                     });
                 }).UseSerilog((context, configuration) =>
--- tests/ch1seL.TonNet.TestsShared/TestsEnv.cs
+++ tests/ch1seL.TonNet.TestsShared/TestsEnv.cs
@@ -12,33 +12,45 @@ namespace ch1seL.TonNet.TestsShared
     internal static class TestsEnv
     {
         private const int DefaultAbiVersion = 2;
-        private const string DefaultTonNetworkAddress = "http://localhost";
+        private const string DefaultTonNetworkAddress = "http://localhost:8080";
         private const string ContractsPath = "_contracts";
 
         public const string LocalGiverAddress = "0:841288ed3b55d9cdafa806807f02a0ae0c169aa5edfe88a789a6482429756a94";
 
-        private static readonly ITonPackageManager TonPackageManager = new FilePackageManager(Options.Create(new FilePackageManagerOptions
-            { PackagesPath = Path.Join(ContractsPath, $"abi_v{CurrentAbiVersion}") }));
+        private static readonly ITonPackageManager TonPackageManager = new FilePackageManager(Options.Create(
+            new FilePackageManagerOptions
+                { PackagesPath = Path.Join(ContractsPath, $"abi_v{CurrentAbiVersion}") }));
 
-        private static readonly ITonPackageManager TonPackageManagerAbi1 = new FilePackageManager(Options.Create(new FilePackageManagerOptions
-            { PackagesPath = Path.Join(ContractsPath, "abi_v1") }));
+        private static readonly ITonPackageManager TonPackageManagerAbi1 = new FilePackageManager(Options.Create(
+            new FilePackageManagerOptions
+                { PackagesPath = Path.Join(ContractsPath, "abi_v1") }));
 
-        public static readonly string TonNetworkAddress = Environment.GetEnvironmentVariable("TON_NETWORK_ADDRESS") ?? DefaultTonNetworkAddress;
+        public static readonly string TonNetworkAddress =
+            Environment.GetEnvironmentVariable("TON_NETWORK_ADDRESS") ?? DefaultTonNetworkAddress;
 
-        public static readonly string SdkVersion = typeof(SdkVersionAttribute).Assembly!.GetCustomAttribute<SdkVersionAttribute>()?.SdkVersion;
+        public static readonly string SdkVersion =
+            typeof(SdkVersionAttribute).Assembly!.GetCustomAttribute<SdkVersionAttribute>()?.SdkVersion;
 
-        private static int CurrentAbiVersion => int.TryParse(Environment.GetEnvironmentVariable("ABI_VERSION"), out var version)
-            ? version == 0 ? DefaultAbiVersion : version
-            : DefaultAbiVersion;
+        private static int CurrentAbiVersion =>
+            int.TryParse(Environment.GetEnvironmentVariable("ABI_VERSION"), out var version)
+                ? version == 0 ? DefaultAbiVersion : version
+                : DefaultAbiVersion;
 
         public static class Packages
         {
             public static readonly Package Events = TonPackageManager.LoadPackage("Events").GetAwaiter().GetResult();
             public static readonly Package Hello = TonPackageManager.LoadPackage("Hello").GetAwaiter().GetResult();
-            public static readonly Package Subscription = TonPackageManager.LoadPackage("Subscription").GetAwaiter().GetResult();
+
+            public static readonly Package Subscription =
+                TonPackageManager.LoadPackage("Subscription").GetAwaiter().GetResult();
+
             public static readonly Abi GiverAbiV1 = TonPackageManagerAbi1.LoadAbi("Giver").GetAwaiter().GetResult();
-            public static readonly Package TestDebotTarget = TonPackageManager.LoadPackage("testDebotTarget").GetAwaiter().GetResult();
-            public static readonly Package TestDebot = TonPackageManager.LoadPackage("testDebot").GetAwaiter().GetResult();
+
+            public static readonly Package TestDebotTarget =
+                TonPackageManager.LoadPackage("testDebotTarget").GetAwaiter().GetResult();
+
+            public static readonly Package TestDebot =
+                TonPackageManager.LoadPackage("testDebot").GetAwaiter().GetResult();
         }
     }
 }
