f1980826a86fc81082d0290e100a293c6dc7ef30
Alexander Salamatov <ch1seL@ya.ru>
2022-01-22 20:13:48 +0300

    feat(Utils) BigInteger.ToDecimalBalance()

---
+++ src/EverscaleNet.Utils/BigIntegerExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Numerics;
+
+namespace EverscaleNet.Utils;
+
+public static class BigIntegerExtensions {
+	public static decimal ToDecimalBalance(this BigInteger bigInteger) {
+		return Math.Round((decimal)BigInteger.Divide(bigInteger, 1_000_000) / 1000, 2);
+	}
+}
