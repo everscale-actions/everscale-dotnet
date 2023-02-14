4a8be4e72726a006e90d87fe8f13280e4c6172a7
Alexander Salamatov <ch1seL@ya.ru>
2023-02-14 23:23:16 +0300

    sync EverClientOptions with documentation

--- src/EverscaleNet/Models/EverClientOptions.cs
+++ src/EverscaleNet/Models/EverClientOptions.cs
@@ -3,10 +3,12 @@ using System.Text.Json.Serialization;
 using EverscaleNet.Client;
 using EverscaleNet.Client.Models;
 
+// ReSharper disable UnusedMember.Global
+
 namespace EverscaleNet.Models;
 
 /// <summary>
-///     Everscale client options. See details https://tonlabs.gitbook.io/ever-sdk/guides/installation/configure_sdk#configure-client
+///     Everscale client options. See details https://docs.everos.dev/ever-sdk/reference/types-and-methods/mod_client#config
 /// </summary>
 public class EverClientOptions {
 	private static readonly BindingConfig DefaultBindingConfig = new() {
@@ -15,27 +17,44 @@ public class EverClientOptions {
 	};
 
 	/// <summary>
-	/// Bindings telemetry
+	///     https://docs.everos.dev/ever-sdk/reference/types-and-methods/mod_client#bindingconfig
 	/// </summary>
 	[JsonPropertyName("binding")]
-	public BindingConfig Binding { get; internal set; } = DefaultBindingConfig;
+	public BindingConfig Binding => DefaultBindingConfig;
 
 	/// <summary>
-	///     https://tonlabs.gitbook.io/ever-sdk/guides/installation/configure_sdk#network-config
-	///     see available endpoint URLs https://tonlabs.gitbook.io/ever-sdk/reference/ton-os-api/networks#networks
+	///     https://docs.everos.dev/ever-sdk/reference/types-and-methods/mod_client#networkconfig
 	/// </summary>
 	[JsonPropertyName("network")]
 	public NetworkConfig Network { get; set; } = new();
 
 	/// <summary>
-	///     https://tonlabs.gitbook.io/ever-sdk/guides/installation/configure_sdk#crypto-config
+	///     https://docs.everos.dev/ever-sdk/reference/types-and-methods/mod_client#cryptoconfig
 	/// </summary>
 	[JsonPropertyName("crypto")]
 	public CryptoConfig Crypto { get; set; } = new();
 
 	/// <summary>
-	///     https://tonlabs.gitbook.io/ever-sdk/guides/installation/configure_sdk#abi-config
+	///     https://docs.everos.dev/ever-sdk/reference/types-and-methods/mod_client#abiconfig
 	/// </summary>
 	[JsonPropertyName("abi")]
 	public AbiConfig Abi { get; set; } = new();
+
+	/// <summary>
+	///     https://docs.everos.dev/ever-sdk/reference/types-and-methods/mod_client#bocconfig
+	/// </summary>
+	[JsonPropertyName("boc")]
+	public BocConfig Boc { get; set; } = new();
+
+	/// <summary>
+	///     https://docs.everos.dev/ever-sdk/reference/types-and-methods/mod_client#proofsconfig
+	/// </summary>
+	[JsonPropertyName("proofs")]
+	public ProofsConfig Proofs { get; set; } = new();
+
+	/// <summary>
+	///     For file based storage is a folder name where SDK will store its data. For browser based is a browser async storage key prefix. Default (recommended) value is "~/.tonclient" for native environments and ".tonclient" for web-browser.
+	/// </summary>
+	[JsonPropertyName("local_storage_path")]
+	public string? LocalStoragePath { get; set; }
 }
