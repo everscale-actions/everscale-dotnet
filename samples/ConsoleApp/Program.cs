433c1263ebb06dd9f47cb53d80a1bbcd7bcbee5d
Alexander Salamatov <ch1seL@ya.ru>
2023-06-14 00:23:26 +0300

    add simple console app example

---
+++ samples/ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using EverscaleNet.Adapter.Rust;
+using EverscaleNet.Client;
+using EverscaleNet.Client.Models;
+using EverscaleNet.Models;
+using Microsoft.Extensions.Options;
+
+var options = new OptionsWrapper<EverClientOptions>(new EverClientOptions());
+await using var adapter = new EverClientRustAdapter(options);
+var client = new EverClient(adapter);
+
+string phrase = (await client.Crypto.MnemonicFromRandom(new ParamsOfMnemonicFromRandom())).Phrase;
+
+Console.WriteLine($"Secret phrase: {phrase}");
